2a38312ba1370d5b2122e66532438ffbe753af65
mitchell <mitchellschaeffer6@gmail.com>
2025-03-10 21:25:41 -0700

    adding login function

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,6 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js";
-
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
 
 const firebaseConfig = {
 
@@ -25,11 +23,11 @@ const firebaseConfig = {
 
 
 const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
+const db = getFirestore(firebaseApp);
+// const analytics = getAnalytics(firebaseApp);
 const auth = getAuth(firebaseApp);
 
 // detect auth state
-
 onAuthStateChanged(auth, user => {
     if (user != null) {
         console.log("logged in");
@@ -38,3 +36,73 @@ onAuthStateChanged(auth, user => {
     }
 
 } );
+
+// Sign up function
+export function signup(first, email, password) {
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // signed up 
+            
+            const user = userCredential.user;
+            console.log("User has sigened up", user);
+            window.location.href = "index.html";
+
+            // store user info and first name
+            // return updateProfile(user, {
+            //   displayName: first
+            // }).then(() => {
+            //   return setDoc(doc(db, "users", user.uid), {
+            //     firstName: first,
+            //     email: email
+            //   });
+            // }).then(() => {
+            //   window.location.href = "index.html";
+            // });
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error("there was a problem signing up: ", errorCode, errorMessage);
+            const errorMessageDiv = document.getElementById('error-message');
+            // Error if email already in use
+                  if (errorCode === 'auth/email-already-in-use') {
+                    errorMessageDiv.textContent = "Email already in use";
+                  } else {
+                    errorMessageDiv.textContent = errorMessage;
+                  }
+              });
+      }
+
+
+
+
+//Login info 
+export function login(email, password) {
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // if login works 
+            const user = userCredential.user;
+            console.log("User is in", user);
+            // redirect feature 
+            window.location.href = "index.html";
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error("there was a problem sign in: ", errorCode, errorMessage);
+
+            // if no account 
+            const errorMessagePlaceholder = document.getElementById('error-message');
+            if (errorCode === 'auth/user-not-found') {
+                if (confirm("No account found under that email, would you like to make one?")) {
+                    window.location.href = "signup.html";
+                }
+            } else if (errorCode === 'auth/invalid-login-credentials') {
+                errorMessagePlaceholder.textContent = "Incorrect password or Email, Please try again";
+            } else {
+                errorMessagePlaceholder.textContent = "There was an error signing in, please try again";
+            }
+        });
+}
+
+
