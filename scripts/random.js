f6a68a8099f6778b874fd60c9a01506533e9133d
mitchell <mitchellschaeffer6@gmail.com>
2025-03-12 18:27:56 -0700

    pushing code to test theory

--- scripts/random.js
+++ scripts/random.js
@@ -1,10 +1,13 @@
 
 function login() {
-    const names = document.getElementById("name").value;
-    const pass = document.getElementById("password").value;
-    
-    console.log("name: "+ names);
-    console.log("pass" + pass);
-    console.log("successfuly logged in");
-    window.location.href = './index.html';
+    document.getElementById('formy').addEventListener('submit', (e) => {
+        e.preventDefault();
+        const names = document.getElementById("name").value;
+        const pass = document.getElementById("password").value;
+        console.log("name: "+ names);
+        console.log("pass" + pass);
+        console.log("successfuly logged in");
+        alert("logged in");
+        window.location.href = '../App/login.html';
+    });
 }
