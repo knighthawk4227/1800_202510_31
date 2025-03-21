1011f3da8244f650027ffedef512ac14e96272d9
Camron Lockyer <clockyer@my.bcit.ca>
2025-03-20 21:31:20 -0700

    created profile page

---
+++ scripts/profile.js
@@ -0,0 +1,4 @@
+document.getElementById('manageGroupBtn').addEventListener('click', function() {
+    const groupInfo = document.getElementById('groupInfo');
+    groupInfo.classList.toggle('active');
+  });
